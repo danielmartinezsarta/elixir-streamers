8ee46e13b07048fff8add0a65f6986ff2ee2fc4f
Daniel Martínez Sarta <daniel@danielmartinez.dev>
2020-04-14 20:12:47 -0400

    Comments about defmodule, import and puts.

--- module_playground.exs
+++ module_playground.exs
@@ -1,5 +1,16 @@
+#defmodule is a macro and module names should be written in PascalCase
 defmodule ModulePlayground do
+
+    #Import is a function, receive the name of the module as first argument and options as second argument.
+    #In the arguments like only: is needed to include the arity of the required function.
+    import IO, only: [puts: 1]
+
+
+    #def keyword is for create a new function inside a module.
     def say_here do
-        IO.puts "I'm Here"
+
+        #puts is a function of IO module that was imported with the import statement.
+        puts "I'm Here"
+        
     end
 end
